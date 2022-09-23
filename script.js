83d60ce73c50172c21fa941dec864a22a7f6cbfc
LucianBlue-D <dhanushpralayakaveri@gmail.com>
2022-09-23 18:31:07 +0530

    Add player selection

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-console.log("Hello")
+// Choices Array 
 const choices = ['ROCK' , 'PAPER' , 'SCISSORS'];
-
-function getRandomItem(){
+let playerInput='';
+let playerSelection='';
+let computerWins = 0;
+let playerWins = 0;
+// Random Choice Function
+function getcomputerChoice(){
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-console.log(getRandomItem())
-console.log(getRandomItem())
+function getPlayerChoice(){
+playerInput = prompt("Make your Choice , Outlander");
+return playerSelection=playerInput.toUpperCase();
+}
+// Computer Choice Selection
+let computerSelection=getcomputerChoice();
+
+// Player Choice Selection
+
+getPlayerChoice();
+
+
+
+
+
+if(choices.includes(playerSelection)){
+    console.log("Atleast you have brain")
+
+}
+else if(playerSelection==''){
+    console.log("Make your choice, FOOL!")
+}
+else{
+    console.log("IDIOT!")
+}
+
+function playRound(playerSelection,computerSelection){
+    if(playerSelection==='ROCK' & computerSelection==='PAPER'){
+
+
+    }
+
+}
+
+
